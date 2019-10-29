67c68733d0b9d8006771fd3843379857f350193f
Benjamin Williams <ben@wecancodeit.org>
2019-10-29 14:12:57 -0400

    Adding comments

---
+++ src/main/java/org/wcci/albums/Comment.java
@@ -0,0 +1,51 @@
+package org.wcci.albums;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Comment {
+    private String content;
+    private String author;
+
+    protected Comment(){}
+    public Comment(String content, String author) {
+
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment)) return false;
+
+        Comment comment = (Comment) o;
+
+        if (getContent() != null ? !getContent().equals(comment.getContent()) : comment.getContent() != null)
+            return false;
+        return getAuthor() != null ? getAuthor().equals(comment.getAuthor()) : comment.getAuthor() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getContent() != null ? getContent().hashCode() : 0;
+        result = 31 * result + (getAuthor() != null ? getAuthor().hashCode() : 0);
+        return result;
+    }
+}
--- src/test/java/org/wcci/albums/JpaWiringTest.java
+++ src/test/java/org/wcci/albums/JpaWiringTest.java
@@ -1,9 +1,5 @@
 package org.wcci.albums;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.contains;
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,34 +7,52 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class JpaWiringTest {
-	
-	@Autowired
-	private ArtistRepository artistRepo;
-	@Autowired
-	private AlbumRepository albumRepo;
-	@Autowired
-	private TestEntityManager entityManager;
-	
 
-	@Test
-	public void artistWillHaveAlbums() throws Exception {
-		Artist testArtist = new Artist("Ben");
-		
-		Album testAlbum1 = new Album("Greatest Hits", testArtist);
-		
-		testArtist = artistRepo.save(testArtist);
-		
-		testAlbum1 = albumRepo.save(testAlbum1);
-		
-		entityManager.flush();
-		entityManager.clear();
-		
-		Artist retrievedArtist = artistRepo.findById(testArtist.getId()).get();
-		
-		assertEquals(testArtist, retrievedArtist);
-		assertThat(retrievedArtist.getAlbums(), contains(testAlbum1));
-	}
+    @Autowired
+    private ArtistRepository artistRepo;
+    @Autowired
+    private AlbumRepository albumRepo;
+    @Autowired
+    private TestEntityManager entityManager;
+
+
+    @Test
+    public void artistWillHaveAlbums() throws Exception {
+        Artist testArtist = new Artist("Ben");
+
+        Album testAlbum1 = new Album("Greatest Hits", testArtist);
+
+        testArtist = artistRepo.save(testArtist);
+
+        testAlbum1 = albumRepo.save(testAlbum1);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Artist retrievedArtist = artistRepo.findById(testArtist.getId()).get();
+
+        assertEquals(testArtist, retrievedArtist);
+        assertThat(retrievedArtist.getAlbums(), contains(testAlbum1));
+    }
+
+    @Test
+    public void artistCanHaveComments() {
+        Artist testArtist = new Artist("Ben");
+        Comment testComment = new Comment("This is an amazing comment!", "BOB");
+        testArtist.addComment(testComment);
+        testArtist = artistRepo.save(testArtist);
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Artist retrievedArtist = artistRepo.findById(testArtist.getId()).get();
+        assertThat(retrievedArtist.getComments(), contains(testComment));
+    }
 }
